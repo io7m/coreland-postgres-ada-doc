2139f2972cacd9f6416d9428d67bb347e5679e7e
m0 <m0@localhost>
2009-06-29 09:57:04 +0100

    Add license.

--- local.sh
+++ local.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
 
+SOURCE="${HOME}/git/coreland/postgres-ada/"
+
+fatal()
+{
+  echo "fatal: $1" 1>&2
+  exit 1
+}
+
+info()
+{
+  echo "info: $1" 1>&2
+}
+
+#
+# Make license.
+#
+
+cat "${SOURCE}/LICENSE" > src/m_license.ud.tmp &&
+  mv src/m_license.ud.tmp src/m_license.ud || fatal "could not create m_license.ud"
